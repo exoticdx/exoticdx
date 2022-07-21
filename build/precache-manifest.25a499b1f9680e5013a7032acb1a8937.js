7db19a2737827555ad2d76b17692a38896e9acfc
exoticdx <96467259+exoticdx@users.noreply.github.com>
2022-07-21 23:13:21 +0530

    hii

---
+++ build/precache-manifest.25a499b1f9680e5013a7032acb1a8937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c78c452384aeb62e5eca",
+    "url": "/exoticdx/static/css/main.ee60d6dc.chunk.css"
+  },
+  {
+    "revision": "c78c452384aeb62e5eca",
+    "url": "/exoticdx/static/js/main.c78c4523.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/exoticdx/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/exoticdx/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "d2c3bffe6d25d221bf1b",
+    "url": "/exoticdx/static/js/runtime~main.d2c3bffe.js"
+  },
+  {
+    "revision": "084cf0f4e4c4429344c16aabf7223a7c",
+    "url": "/exoticdx/index.html"
+  }
+];
